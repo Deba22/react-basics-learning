ae85d48607a82d2f1a22e310e3c4cdb3d8d91922
Debasish Gracias <d.gracias@clerkswell.com>
2021-02-14 15:49:43 +0000

    more learnings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from './Home';
 import Navbar from './Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title = 'Welcome to new blog';
@@ -18,6 +20,12 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function BlogList({ blogs, title }) {
 
@@ -8,9 +9,11 @@ function BlogList({ blogs, title }) {
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    {/* <button onClick={() => handleDelete(blog.id)}>Delete blog</button> */}
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                        {/* <button onClick={() => handleDelete(blog.id)}>Delete blog</button> */}
+                    </Link>
                 </div>
             ))}
         </div>
